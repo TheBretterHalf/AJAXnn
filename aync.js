1af6f4a05193e5c1519807eed089079cca070284
Brett Stainken <brettstainken.student@careerdevs.com>
2019-01-31 03:50:14 +0000

    AJAX

---
+++ aync.js
@@ -0,0 +1,28 @@
+window.onload = function(){
+    
+  var http = new XMLHttpRequest();
+  
+  http.onreadystatechange = function(){
+      if(http.readyState == 4 && http.status == 200){
+          //console.log(JSON.parse(http.response));
+      }
+  };
+  
+  http.open("GET", "dataz/tweets.json" , true);
+  
+  http.send();
+  
+  //jquery method
+  $.get("data/tweets.json", function(data){
+     console.log(data); 
+  });
+  console.log("test");
+};
+/* Ready States
+
+0-request not initialized
+1-request has been set up
+2-request has been sent
+3-request is in process
+4-request is complete
+*/
